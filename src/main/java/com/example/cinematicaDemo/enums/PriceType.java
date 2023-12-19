67bb112e2e43adc4192b6319e15be8983df72e58
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-12-19 19:07:20 +0600

    add enums

---
+++ src/main/java/com/example/cinematicaDemo/enums/PriceType.java
@@ -0,0 +1,8 @@
+package com.example.cinematicaDemo.enums;
+
+public enum PriceType {
+
+    CHILD,
+    STANDARD,
+
+}
---
+++ src/main/java/com/example/cinematicaDemo/enums/SeatStatus.java
@@ -0,0 +1,6 @@
+package com.example.cinematicaDemo.enums;
+
+public enum SeatStatus {
+    FREE,
+    RESERVED,
+}
